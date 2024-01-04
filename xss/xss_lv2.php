abc8a37b62d44d7ba52305673198adb5a33d46ee
namchamchi <90975762+namchamchi@users.noreply.github.com>
2024-01-04 20:04:09 +0700

    chặn tấn công cơ bản

--- xss/xss_lv2.php
+++ xss/xss_lv2.php
@@ -8,16 +8,30 @@ $con = new PDO('mysql:host=localhost;dbname=db_webpbl6', 'root', '');
 $commentObject = $con->query("SELECT * FROM comments_2");
 $commentObject->setFetchMode(PDO::FETCH_OBJ);
 if (isset($_POST['submit'])) {
-    //code lỗi
-    $name = $_POST['name'];
+    // //code lỗi
+    // $name = $_POST['name'];
+    // $body =  htmlspecialchars($_POST['body'], ENT_QUOTES);
+    // $body = str_replace("<script>", "",$_POST["body"]);
+    // $commentQuery = $con->prepare("INSERT INTO comments_2(name, body) VALUES(:name, :body)");
+    // $commentQuery->execute(['name' => $name, 'body' => $body]);
 
-    // Sử dụng htmlspecialchars để mã hóa các ký tự đặc biệt trong dữ liệu người dùng
-    $body =  htmlspecialchars($_POST['body'], ENT_QUOTES);
+    $name = htmlspecialchars($_POST['name'], ENT_QUOTES); // Chuyển đổi ký tự đặc biệt thành HTML entities
+    $body = htmlspecialchars($_POST['body'], ENT_QUOTES); // Chuyển đổi ký tự đặc biệt thành HTML entities
 
-    $body = str_replace("<script>", "",$_POST["body"]);
-    $commentQuery = $con->prepare("INSERT INTO comments_2(name, body) VALUES(:name, :body)");
-    $commentQuery->execute(['name' => $name, 'body' => $body]);
+    // Kiểm tra và xử lý dữ liệu trước khi lưu vào cơ sở dữ liệu
+    if (!empty($name) && !empty($body)) {
+        // Tạo prepared statement
+        $commentQuery = $con->prepare("INSERT INTO comments_2 (name, body) VALUES (:name, :body)");
 
+        // Bind dữ liệu và thực thi truy vấn
+        $commentQuery->execute(['name' => $name, 'body' => $body]);
+
+        // Thông báo lưu thành công hoặc điều chỉnh code để xử lý khi lưu dữ liệu thành công
+        echo "Lưu dữ liệu thành công!";
+    } else {
+        // Xử lý khi dữ liệu không hợp lệ, ví dụ: hiển thị thông báo lỗi
+        echo "Vui lòng điền đầy đủ thông tin!";
+    }
 }
 ?>
 <!DOCTYPE html>
